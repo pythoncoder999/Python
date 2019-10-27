8708425ceea00c9a67dcf5b3f14dfbff3be450d5
Gunardi Saputra <pythoncoder999@gmail.com>
2019-10-26 23:24:49 -0500

    Adding helloFunction.py

---
+++ AutomateTheBoringStaff/helloFunction.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 26 23:16:59 2019
+
+@author: Gunardi Saputra
+"""
+
+def hello():
+    print("Ni hao!")
+    print("Genki desu ka?")
+    print("Apa kabar?")
+    
+hello()
+  

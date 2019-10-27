db70622812e2cc01ef914a6c5ab72bc57f3d5b93
Gunardi Saputra <pythoncoder999@gmail.com>
2019-10-27 00:39:02 -0500

    Adding allMyDogs.py

---
+++ AutomateTheBoringStaff/allMyDogs.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 27 00:30:31 2019
+
+
+dogName1 = "One"
+dogName2 = "Two"
+dogName3 = "Three"
+dogName4 = "Four"
+dogName5 = "Five"
+dogName6 = "Six"
+
+@author: Gunardi Saputra
+"""
+
+
+print("Enter the name of dog 1: ")
+dogName1 = input()
+print("Enter the name of dog 2: ")
+dogName2 = input()
+print("Enter the name of dog 3: ")
+dogName3 = input()
+print("Enter the name of dog 4: ")
+dogName4 = input()
+print("Enter the name of dog 5: ")
+dogName5 = input()
+print("Enter the name of dog 6: ")
+dogName6 = input()
+
+print("The dog names are: " + dogName1 + " "
+      + dogName2 + " "
+      + dogName3 + " "
+      + dogName4 + " "
+      + dogName5 + " "
+      + dogName6 + " ")

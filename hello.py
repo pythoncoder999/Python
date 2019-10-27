4b51883e0a70e9ba242ef3902fcb5685a1195ab3
Gunardi Saputra <pythoncoder999@gmail.com>
2019-10-26 21:55:56 -0500

    adding hello.py and monster.py

---
+++ hello.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 26 21:51:43 2019
+
+@author: Gunardi Saputra
+"""
+print("Hello world")
+print("What is your age?") #ask for their age
+
+myAge = input()
+
+print()
